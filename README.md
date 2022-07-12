3969a0e62f998fc6808bec3be068a34b59fafb0d
Hannah.Celemen <hcelemen@icloud.com>
2022-07-11 18:33:12 -0600

    feat: add object

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #Let's work on some git workflow!
+- `git branch` will create new branch
+- `git checkout` will take you take you to a specified branch
+- `git checkout -b` combines the two commands above
---
+++ index.js
@@ -0,0 +1,17 @@
+let ferret = {
+    name: "Lyra",
+    age: 4,
+    color: calico
+    favHobbies: [
+        {
+            activity: "hoarding", 
+            things: ["tubes", "shoes", "stuff toys"]
+        }, {
+            activity: "climbing",
+            things: ["couch", "cage", "table"]
+        }
+    ]
+    getExcited: function() {
+        return "Dook! Dook!"
+    }
+}
